21ec77dd402c077f14e0a4853596513cc4b80771
khoaDLuu <dangkhoa240899@gmail.com>
2021-10-03 19:51:39 +0700

    Add docker compose for Postgres

--- foodalways/django_food/settings.py
+++ foodalways/django_food/settings.py
@@ -108,12 +108,12 @@ WSGI_APPLICATION = 'django_food.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', ''),
         'USER': os.getenv('DB_USERNAME', ''),
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
